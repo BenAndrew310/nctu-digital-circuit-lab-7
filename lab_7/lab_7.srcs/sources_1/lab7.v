9be39f3a8c51b24d3e29dffedf523f32eedd51a2
BenAndrew310 <andreben2442@gmail.com>
2020-11-23 00:47:12 +0800

    Sunday night home session

--- lab_7/lab_7.srcs/sources_1/lab7.v
+++ lab_7/lab_7.srcs/sources_1/lab7.v
@@ -39,23 +39,25 @@ module lab6(
   output uart_tx
 );
 
-localparam [1:0] S_MAIN_ADDR = 3'b000, S_MAIN_READ = 3'b001,
-                 S_MAIN_SHOW = 3'b010, S_MAIN_WAIT = 3'b011;
+localparam [2:0] S_MAIN_ADDR = 3'b000, S_MAIN_READ = 3'b001,
+                 S_MAIN_SHOW = 3'b010, S_MAIN_WAIT = 3'b011,
+                 S_MAIN_COMPUTE = 3'b100, S_MAIN_UART = 3'b101;
+                 S_MAIN_UART_DISPLAY = 3'b110;
 localparam [1:0] S_UART_IDLE = 0, S_UART_WAIT = 1,
                  S_UART_SEND = 2, S_UART_INCR = 3;
 
-localparam l1_len = 35;
+localparam l1_len = 34;
 localparam l2_len = 6;
-localparam l3_len = 33;
-localparam l4_len = 33;
-localparam l5_len = 33;
+localparam l3_len = 32;
+localparam l4_len = 32;
+localparam l5_len = 32;
 localparam l6_len = 33;
 localparam MEM_SIZE = l1_len + l2_len + l3_len + l4_len + l5_len + l6_len;
 
 // declare system variables
 wire [1:0]  btn_level, btn_pressed;
 reg  [1:0]  prev_btn_level;
-reg  [1:0]  P, P_next;
+reg  [2:0]  P, P_next;
 reg  [11:0] user_addr;
 reg  [7:0]  user_data;
 
@@ -66,21 +68,29 @@ reg [$clog2(MEM_SIZE):0] send_counter;
 reg [7:0] data[0:MEM_SIZE-1];
 reg Q, Q_next;
 
-reg [0:l1_len*8-1] line1 = {"\015\012The matrix multiplication result",8'h00};
+reg [0:l1_len*8-1] line1 = {"\015\012The matrix multiplication result"};
 reg [0:l2_len*8-1] line2 = {"\015\012is:",8'h00};
-reg [0:l3_len*8-1] line3 = {"\015\012[ -----, -----, -----, ----- ]",8'h00};
-reg [0:l4_len*8-1] line4 = {"\015\012[ -----, -----, -----, ----- ]",8'h00};
-reg [0:l5_len*8-1] line5 = {"\015\012[ -----, -----, -----, ----- ]",8'h00};
-reg [0:l6_len*8-1] line6 = {"\015\012[ -----, -----, -----, ----- ]",8'h00};
+reg [0:l3_len*8-1] line3 = {"\015\012[ -----, -----, -----, ----- ]"}; // start index 40
+reg [0:l4_len*8-1] line4 = {"\015\012[ -----, -----, -----, ----- ]"}; // start index 72
+reg [0:l5_len*8-1] line5 = {"\015\012[ -----, -----, -----, ----- ]"}; // start index 104
+reg [0:l6_len*8-1] line6 = {"\015\012[ -----, -----, -----, ----- ]",8'h00}; // start index 136
+
+integer idx,idx1,idx2,idx3;
+integer p1,p2,p3,p4;
+reg [7:0] mtx1[0:15];
+reg [7:0] mtx2[0:15]; 
+reg [17:0] mtx3[0:15]; // result matrix
+reg [17:0] pipe1[0:3], pipe2[0:3], pipe3[0:3], pipe4[0:3];
+wire result_ready = 0;
+reg mtx1_ready = 0;
+reg mtx2_ready = 0;
+reg pipe1_ready, pipe2_ready, pipe3_ready, pipe4_ready;
 
 // declare UART signals
 wire transmit;
 wire received;
 wire [7:0] rx_byte;
-//reg  [7:0] rx_temp;  // if received is true, rx_temp latches rx_byte for ONLY ONE CLOCK CYCLE!
 wire [7:0] tx_byte;
-//wire [7:0] echo_key; // keystrokes to be echoed to the terminal
-//wire is_num_key;
 wire is_receiving;
 wire is_transmitting;
 wire recv_error;
@@ -91,7 +101,7 @@ wire [7:0]  data_in;
 wire [7:0]  data_out;
 wire        sram_we, sram_en;
 
-assign usr_led = 4'h00;
+assign usr_led = P;
 
 LCD_module lcd0( 
   .clk(clk),
@@ -151,7 +161,7 @@ sram ram0(.clk(clk), .we(sram_we), .en(sram_en),
 assign sram_we = usr_btn[3]; // In this demo, we do not write the SRAM. However,
                              // if you set 'we' to 0, Vivado fails to synthesize
                              // ram0 as a BRAM -- this is a bug in Vivado.
-assign sram_en = (P == S_MAIN_ADDR || P == S_MAIN_READ); // Enable the SRAM block.
+assign sram_en = (P == S_MAIN_ADDR || P == S_MAIN_READ || P == S_MAIN_WAIT); // Enable the SRAM block.
 assign sram_addr = user_addr[11:0];
 assign data_in = 8'b0; // SRAM is read-only so we tie inputs to zeros.
 // End of the SRAM memory block.
@@ -175,10 +185,21 @@ always @(*) begin // FSM next-state logic
     S_MAIN_READ: // fetch the sample from the SRAM
       P_next = S_MAIN_SHOW;
     S_MAIN_SHOW:
-      P_next = S_MAIN_WAIT;
+      if (btn_pressed[1]) P_next = S_MAIN_WAIT;
+      else P_next = S_MAIN_SHOW;
     S_MAIN_WAIT: // wait for a button click
-      if (| btn_pressed == 1) P_next = S_MAIN_ADDR;
+      // if (| btn_pressed == 1) P_next = S_MAIN_ADDR;
+      // else P_next = S_MAIN_WAIT;
+      if (mtx1_ready && mtx2_ready) P_next = S_MAIN_COMPUTE;
       else P_next = S_MAIN_WAIT;
+    S_MAIN_COMPUTE:
+      if (result_ready) P_next = S_MAIN_UART;
+      else P_next = S_MAIN_COMPUTE;
+    S_MAIN_UART:
+      P_next = S_MAIN_UART_DISPLAY;
+    S_MAIN_UART_DISPLAY:
+      if (print_done) P_next = S_MAIN_ADDR; 
+      P_next = S_MAIN_UART_DISPLAY;
   endcase
 end
 
@@ -192,39 +213,39 @@ end
 
 // ------------------------------------------------------------------------
 // The following code updates the 1602 LCD text messages.
-always @(posedge clk) begin
-  if (~reset_n) begin
-    row_A <= "Data at [0x---] ";
-  end
-  else if (P == S_MAIN_SHOW) begin
-    row_A[39:32] <= ((user_addr[11:08] > 9)? "7" : "0") + user_addr[11:08];
-    row_A[31:24] <= ((user_addr[07:04] > 9)? "7" : "0") + user_addr[07:04];
-    row_A[23:16] <= ((user_addr[03:00] > 9)? "7" : "0") + user_addr[03:00];
-  end
-end
+// always @(posedge clk) begin
+//   if (~reset_n) begin
+//     row_A <= "Data at [0x---] ";
+//   end
+//   else if (P == S_MAIN_SHOW) begin
+//     row_A[39:32] <= ((user_addr[11:08] > 9)? "7" : "0") + user_addr[11:08];
+//     row_A[31:24] <= ((user_addr[07:04] > 9)? "7" : "0") + user_addr[07:04];
+//     row_A[23:16] <= ((user_addr[03:00] > 9)? "7" : "0") + user_addr[03:00];
+//   end
+// end
 
-always @(posedge clk) begin
-  if (~reset_n) begin
-    row_B <= "is equal to 0x--";
-  end
-  else if (P == S_MAIN_SHOW) begin
-    row_B[15:08] <= ((user_data[7:4] > 9)? "7" : "0") + user_data[7:4];
-    row_B[07: 0] <= ((user_data[3:0] > 9)? "7" : "0") + user_data[3:0];
-  end
-end
+// always @(posedge clk) begin
+//   if (~reset_n) begin
+//     row_B <= "is equal to 0x--";
+//   end
+//   else if (P == S_MAIN_SHOW) begin
+//   end
+//     row_B[15:08] <= ((user_data[7:4] > 9)? "7" : "0") + user_data[7:4];
+//     row_B[07: 0] <= ((user_data[3:0] > 9)? "7" : "0") + user_data[3:0];
+// end
 // End of the 1602 LCD text-updating code.
 // ------------------------------------------------------------------------
 
 // ------------------------------------------------------------------------
 // The circuit block that processes the user's button event.
-always @(posedge clk) begin
-  if (~reset_n)
-    user_addr <= 12'h000;
-  else if (btn_pressed[1])
-    user_addr <= (user_addr < 2048)? user_addr + 1 : user_addr;
-  else if (btn_pressed[0])
-    user_addr <= (user_addr > 0)? user_addr - 1 : user_addr;
-end
+// always @(posedge clk) begin
+//   if (~reset_n)
+//     user_addr <= 12'h000;
+//   else if (btn_pressed[1])
+//     user_addr <= (user_addr < 2048)? user_addr + 1 : user_addr;
+//   else if (btn_pressed[0])
+//     user_addr <= (user_addr > 0)? user_addr - 1 : user_addr;
+// end
 // End of the user's button control.
 // ------------------------------------------------------------------------
 // FSM output logics: UART transmission control signals
@@ -256,8 +277,211 @@ always @(*) begin // FSM next-state logic
   endcase
 end
 
+assign print_enable = (P == S_MAIN_UART);
+assign print_done = (tx_byte == 8'h0);
 assign transmit = (Q_next == S_UART_WAIT || print_enable);
-assign tx_byte = (received) ? 0 : data[send_counter]; 
+assign tx_byte = (received) ? 0 : data[send_counter];
+assign result_ready = (pipe1_ready && pipe2_ready && pipe3_ready && pipe4_ready);
 
+always @(posedge clk) begin
+  case (P_next)
+    S_MAIN_SHOW: send_counter <= 0;
+    S_MAIN_UART_DISPLAY: send_counter <= 40;
+    default: send_counter <= send_counter + (Q_next == S_UART_INCR);
+  endcase
+end
+
+always @(posedge clk) begin
+  if (~reset_n) begin
+    {data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], 
+     data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], 
+     data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24], 
+     data[25], data[26], data[27], data[28], data[29], data[30], data[31], data[32], 
+     data[33]}
+    <= {line1} ;
+
+    {data[34], data[35], data[36], data[37], data[38], data[39]} 
+    <= {line2};
+
+    {data[40], data[41], data[42], data[43], data[44], data[45], data[46], data[47], data[48], 
+     data[49], data[50], data[51], data[52], data[53], data[54], data[55], data[56], 
+     data[57], data[58], data[59], data[60], data[61], data[62], data[63], data[64], 
+     data[65], data[66], data[67], data[68], data[69], data[70], data[71]}
+    <= {line3};
+
+    {data[72], data[73], data[74], data[75], data[76], data[77], data[78], data[79], data[80], data[81], 
+     data[82], data[83], data[84], data[85], data[86], data[87], data[88], data[89], 
+     data[90], data[91], data[92], data[93], data[94], data[95], data[96], data[97], 
+     data[98], data[99], data[100], data[101], data[102], data[103]}
+    <= {line4};
+
+    {data[104], data[105], data[106], data[107], data[108], data[109], data[110], data[111], 
+     data[112], data[113], data[114], data[115], data[116], data[117], data[118], data[119], 
+     data[120], data[121], data[122], data[123], data[124], data[125], data[126], data[127], 
+     data[128], data[129], data[130], data[131], data[132], data[133], data[134], data[135]}
+    <= {line5};
+
+    {data[136], data[137], data[138], 
+     data[139], data[140], data[141], data[142], data[143], data[144], data[145], data[146], data[147], 
+     data[148], data[149], data[150], data[151], data[152], data[153], data[154], data[155],
+     data[156], data[157], data[158], data[159], data[160], data[161], data[162], data[163], 
+     data[164], data[165], data[166], data[167], data[168]}
+     <= {line6};
+  end
+  else if (P == S_MAIN_UART_DISPLAY) begin
+    for (idx=0; idx<4; idx=idx+1) begin
+      data[45+idx*33] <= ((mtx3[idx*4+0][17:16] > 9) ? "7" : "0") + mtx3[idx*4+0][17:16];
+      data[46+idx*33] <= ((mtx3[idx*4+0][15:12] > 9) ? "7" : "0") + mtx3[idx*4+0][15:12];
+      data[47+idx*33] <= ((mtx3[idx*4+0][11: 8] > 9) ? "7" : "0") + mtx3[idx*4+0][11: 8];
+      data[48+idx*33] <= ((mtx3[idx*4+0][ 7: 4] > 9) ? "7" : "0") + mtx3[idx*4+0][ 7: 4];
+      data[49+idx*33] <= ((mtx3[idx*4+0][ 3: 0] > 9) ? "7" : "0") + mtx3[idx*4+0][ 3: 0];
+
+      data[52+idx*33] <= ((mtx3[idx*4+1][17:16] > 9) ? "7" : "0") + mtx3[idx*4+1][17:16];
+      data[53+idx*33] <= ((mtx3[idx*4+1][15:12] > 9) ? "7" : "0") + mtx3[idx*4+1][15:12];
+      data[54+idx*33] <= ((mtx3[idx*4+1][11: 8] > 9) ? "7" : "0") + mtx3[idx*4+1][11: 8];
+      data[55+idx*33] <= ((mtx3[idx*4+1][ 7: 4] > 9) ? "7" : "0") + mtx3[idx*4+1][ 7: 4];
+      data[56+idx*33] <= ((mtx3[idx*4+1][ 3: 0] > 9) ? "7" : "0") + mtx3[idx*4+1][ 3: 0];
+
+      data[59+idx*33] <= ((mtx3[idx*4+2][17:16] > 9) ? "7" : "0") + mtx3[idx*4+2][17:16];
+      data[60+idx*33] <= ((mtx3[idx*4+2][15:12] > 9) ? "7" : "0") + mtx3[idx*4+2][15:12];
+      data[61+idx*33] <= ((mtx3[idx*4+2][11: 8] > 9) ? "7" : "0") + mtx3[idx*4+2][11: 8];
+      data[62+idx*33] <= ((mtx3[idx*4+2][ 7: 4] > 9) ? "7" : "0") + mtx3[idx*4+2][ 7: 4];
+      data[63+idx*33] <= ((mtx3[idx*4+2][ 3: 0] > 9) ? "7" : "0") + mtx3[idx*4+2][ 3: 0];
+
+      data[66+idx*33] <= ((mtx3[idx*4+3][17:16] > 9) ? "7" : "0") + mtx3[idx*4+3][17:16];
+      data[67+idx*33] <= ((mtx3[idx*4+3][15:12] > 9) ? "7" : "0") + mtx3[idx*4+3][15:12];
+      data[68+idx*33] <= ((mtx3[idx*4+3][11: 8] > 9) ? "7" : "0") + mtx3[idx*4+3][11: 8];
+      data[69+idx*33] <= ((mtx3[idx*4+3][ 7: 4] > 9) ? "7" : "0") + mtx3[idx*4+3][ 7: 4];
+      data[70+idx*33] <= ((mtx3[idx*4+3][ 3: 0] > 9) ? "7" : "0") + mtx3[idx*4+3][ 3: 0];
+    end
+  end
+end 
+
+always @(posedge clk) begin
+  if (~reset_n) begin
+    result_ready <= 0;
+    // mtx1 <= 0;
+    // mtx2 <= 0;
+    // mtx3 <= 0;
+    user_addr <= 12'h000;
+    idx1 <= 0;
+    idx2 <= 0;
+    // idx3 <= 0;
+  end
+  else if (P == S_MAIN_WAIT) begin
+    if (!mtx1_ready) begin
+      mtx1[idx1] <= user_data;
+      idx1 = idx1+1;
+      if (idx1==16) mtx1_ready <= 1;
+    end
+    else if (!mtx2_ready && mtx1_ready) begin
+      mtx2[idx2] <= user_data;
+      idx2 = idx2+1;
+      if (idx2==16) mtx2_ready <= 1;
+    end
+    user_addr = user_addr + 1;
+  end
+  else if (P == S_MAIN_UART) begin
+    {mtx3[ 0], mtx3[ 1], mtx3[ 2], mtx3[ 3], mtx3[ 4], mtx3[ 5], mtx3[ 6], mtx3[ 7], 
+     mtx3[ 8], mtx3[ 9], mtx3[10], mtx3[11], mtx3[12], mtx3[13], mtx3[14], mtx3[15]}
+    <= {pipe1[0], pipe1[1], pipe1[2], pipe1[3],
+        pipe2[0], pipe2[1], pipe2[2], pipe2[3],
+        pipe3[0], pipe3[1], pipe3[2], pipe3[3],
+        pipe4[0], pipe4[1], pipe4[2], pipe4[3]};
+  end
+  // else if (P == S_MAIN_COMPUTE) begin
+  //   case (idx3)
+  //      0: mtx3[idx3] <= mtx1[0]*mtx2[0] + mtx1[1]*mtx2[4] + mtx1[2]*mtx2[ 8] + mtx1[3]*mtx2[12];
+  //      1: mtx3[idx3] <= mtx1[0]*mtx2[1] + mtx1[1]*mtx2[5] + mtx1[2]*mtx2[ 9] + mtx1[3]*mtx2[13];
+  //      2: mtx3[idx3] <= mtx1[0]*mtx2[2] + mtx1[1]*mtx2[6] + mtx1[2]*mtx2[10] + mtx1[3]*mtx2[14];
+  //      3: mtx3[idx3] <= mtx1[0]*mtx2[3] + mtx1[1]*mtx2[7] + mtx1[2]*mtx2[11] + mtx1[3]*mtx2[15];
+
+  //      4: mtx3[idx3] <= mtx1[4]*mtx2[0] + mtx1[5]*mtx2[4] + mtx1[6]*mtx2[ 8] + mtx1[7]*mtx2[12];
+  //      5: mtx3[idx3] <= mtx1[4]*mtx2[1] + mtx1[5]*mtx2[5] + mtx1[6]*mtx2[ 9] + mtx1[7]*mtx2[13];
+  //      6: mtx3[idx3] <= mtx1[4]*mtx2[2] + mtx1[5]*mtx2[6] + mtx1[6]*mtx2[10] + mtx1[7]*mtx2[14];
+  //      7: mtx3[idx3] <= mtx1[4]*mtx2[3] + mtx1[5]*mtx2[7] + mtx1[6]*mtx2[11] + mtx1[7]*mtx2[15];
+
+  //      8: mtx3[idx3] <= mtx1[8]*mtx2[0] + mtx1[9]*mtx2[4] + mtx1[10]*mtx2[ 8] + mtx1[11]*mtx2[12];
+  //      9: mtx3[idx3] <= mtx1[8]*mtx2[1] + mtx1[9]*mtx2[5] + mtx1[10]*mtx2[ 9] + mtx1[11]*mtx2[13];
+  //     10: mtx3[idx3] <= mtx1[8]*mtx2[2] + mtx1[9]*mtx2[6] + mtx1[10]*mtx2[10] + mtx1[11]*mtx2[14];
+  //     11: mtx3[idx3] <= mtx1[8]*mtx2[3] + mtx1[9]*mtx2[7] + mtx1[10]*mtx2[11] + mtx1[11]*mtx2[15];
+
+  //     12: mtx3[idx3] <= mtx1[12]*mtx2[0] + mtx1[13]*mtx2[4] + mtx1[14]*mtx2[ 8] + mtx1[15]*mtx2[12];
+  //     13: mtx3[idx3] <= mtx1[12]*mtx2[1] + mtx1[13]*mtx2[5] + mtx1[14]*mtx2[ 9] + mtx1[15]*mtx2[13];
+  //     14: mtx3[idx3] <= mtx1[12]*mtx2[2] + mtx1[13]*mtx2[6] + mtx1[14]*mtx2[10] + mtx1[15]*mtx2[14];
+  //     15: mtx3[idx3] <= mtx1[12]*mtx2[3] + mtx1[13]*mtx2[7] + mtx1[14]*mtx2[11] + mtx1[15]*mtx2[15];
+  //   endcase
+  //   idx3 = idx3+1;
+  //   if (idx3>=16) result_ready <= 1;
+  // end
+end
+
+always @(posedge clk) begin
+  if (~reset_n) begin
+    p1 <= 0;
+    pipe1_ready <= 0;
+  end
+  else if (P == S_MAIN_COMPUTE && !pipe1_ready) begin
+    case (p1):
+      0: pipe1[p1] <= mtx1[0]*mtx2[0] + mtx1[1]*mtx2[4] + mtx1[2]*mtx2[ 8] + mtx1[3]*mtx2[12];
+      1: pipe1[p1] <= mtx1[0]*mtx2[1] + mtx1[1]*mtx2[5] + mtx1[2]*mtx2[ 9] + mtx1[3]*mtx2[13];
+      2: pipe1[p1] <= mtx1[0]*mtx2[2] + mtx1[1]*mtx2[6] + mtx1[2]*mtx2[10] + mtx1[3]*mtx2[14];
+      3: pipe1[p1] <= mtx1[0]*mtx2[3] + mtx1[1]*mtx2[7] + mtx1[2]*mtx2[11] + mtx1[3]*mtx2[15];
+    endcase
+    p1 = p1+1;
+    if (p1>=4) pipe1_ready = 1;
+  end
+end
+
+always @(posedge clk) begin
+  if (~reset_n) begin
+    p2 <= 0;
+    pipe2_ready <= 0;
+  end
+  else if (P == S_MAIN_COMPUTE && !pipe2_ready) begin
+    case (p2):
+      0: pipe2[p2] <= mtx1[4]*mtx2[0] + mtx1[5]*mtx2[4] + mtx1[6]*mtx2[ 8] + mtx1[7]*mtx2[12];
+      1: pipe2[p2] <= mtx1[4]*mtx2[1] + mtx1[5]*mtx2[5] + mtx1[6]*mtx2[ 9] + mtx1[7]*mtx2[13];
+      2: pipe2[p2] <= mtx1[4]*mtx2[2] + mtx1[5]*mtx2[6] + mtx1[6]*mtx2[10] + mtx1[7]*mtx2[14];
+      3: pipe2[p2] <= mtx1[4]*mtx2[3] + mtx1[5]*mtx2[7] + mtx1[6]*mtx2[11] + mtx1[7]*mtx2[15];
+    endcase
+    p2 = p2+1;
+    if (p2>=4) pipe2_ready = 1;
+  end
+end
+
+always @(posedge clk) begin
+  if (~reset_n) begin
+    p3 <= 0;
+    pipe3_ready <= 0;
+  end
+  else if (P == S_MAIN_COMPUTE && !pipe3_ready) begin
+    case (p3):
+      0: pipe3[p3] <= mtx1[8]*mtx2[0] + mtx1[9]*mtx2[4] + mtx1[10]*mtx2[ 8] + mtx1[11]*mtx2[12];
+      1: pipe3[p3] <= mtx1[8]*mtx2[1] + mtx1[9]*mtx2[5] + mtx1[10]*mtx2[ 9] + mtx1[11]*mtx2[13];
+      2: pipe3[p3] <= mtx1[8]*mtx2[2] + mtx1[9]*mtx2[6] + mtx1[10]*mtx2[10] + mtx1[11]*mtx2[14];
+      3: pipe3[p3] <= mtx1[8]*mtx2[3] + mtx1[9]*mtx2[7] + mtx1[10]*mtx2[11] + mtx1[11]*mtx2[15];
+    endcase
+    p3 = p3+1;
+    if (p3>=4) pipe3_ready = 1;
+  end
+end
+
+always @(posedge clk) begin
+  if (~reset_n) begin
+    p4 <= 0;
+    pipe4_ready <= 0;
+  end
+  else if (P == S_MAIN_COMPUTE && !pipe4_ready) begin
+    case (p4):
+      0: pipe4[p4] <= mtx1[12]*mtx2[0] + mtx1[13]*mtx2[4] + mtx1[14]*mtx2[ 8] + mtx1[15]*mtx2[12];
+      1: pipe4[p4] <= mtx1[12]*mtx2[1] + mtx1[13]*mtx2[5] + mtx1[14]*mtx2[ 9] + mtx1[15]*mtx2[13];
+      2: pipe4[p4] <= mtx1[12]*mtx2[2] + mtx1[13]*mtx2[6] + mtx1[14]*mtx2[10] + mtx1[15]*mtx2[14];
+      3: pipe4[p4] <= mtx1[12]*mtx2[3] + mtx1[13]*mtx2[7] + mtx1[14]*mtx2[11] + mtx1[15]*mtx2[15];
+    endcase
+    p4 = p4+1;
+    if (p4>=4) pipe4_ready = 1;
+  end
+end
 
 endmodule
